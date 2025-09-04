ff0f42f903e055aaaa0db8fcdc2eeb5d2dfe70ac
Ankush-Sonar <ankushsonar31@goggle.com>
2025-09-04 21:10:46 +0530

    main.py commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from extract import Extract
+from transform import Transform
+
+if __name__ == "__main__":
+    path = "data/Flight_data.csv"
+    extractor = Extract()
+    data= extractor.extract_info(path)
+    
+    transform= Transform()
+    data = transform.transform_data(data)
+    print(data.head())  # Display the first few rows of the extracted data
